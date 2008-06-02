587f6e9145adc4edd0bb3c003a870595a43f9774
Wisling <wisling@example.com>
2008-06-02 03:46:34 +0000

    changed default to 'external' container

--- mods/mod_looter/ConfigData.cpp
+++ mods/mod_looter/ConfigData.cpp
@@ -23,16 +23,16 @@ CConfigData::CConfigData()
 	m_lootFood=0;
 	m_lootCustom=0;
 	m_autoOpen=0;
-	m_mode10=0;
-	m_mode9=0;
-	m_mode8=0;
-	m_mode7=0;
-	m_mode6=0;
-	m_mode5=0;
-	m_mode4=0;
-	m_mode3=0;
-	m_mode2=0;
-	m_mode1=0;
+	m_mode10=2;
+	m_mode9=2;
+	m_mode8=2;
+	m_mode7=2;
+	m_mode6=2;
+	m_mode5=2;
+	m_mode4=2;
+	m_mode3=2;
+	m_mode2=2;
+	m_mode1=1;
 }
 
 CConfigData::~CConfigData()
