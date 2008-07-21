188dd2ce745ce400d29c546bcd16dfb27f188eab
Akilez <akilez@example.com>
2008-07-21 11:14:11 +0000

    First release of new module:  Auto sort

---
+++ mods/mod_sorter/ConfigData.h
@@ -0,0 +1,28 @@
+// ConfigData.h: interface for the CConfigData class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_CONFIGDATA_H__3C69E44D_D036_45EB_BCE8_5358CF410631__INCLUDED_)
+#define AFX_CONFIGDATA_H__3C69E44D_D036_45EB_BCE8_5358CF410631__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+struct slot {
+	char itemName[64];
+};
+
+struct container {
+	slot slotNr[32];
+};
+
+class CConfigData {
+public:
+	CConfigData();
+	virtual ~CConfigData();
+	container sortBags[8];
+
+};
+
+#endif // !defined(AFX_CONFIGDATA_H__3C69E44D_D036_45EB_BCE8_5358CF410631__INCLUDED_)
---
+++ mods/mod_sorter/ConfigDialog.h
@@ -0,0 +1,86 @@
+#if !defined(AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_)
+#define AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ConfigDialog.h : header file
+//
+
+#include "mod_sorter.h"
+#include "ConfigData.h"
+
+class CMod_sorterApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CConfigDialog dialog
+
+class CConfigDialog : public CDialog
+{
+// Construction
+public:
+	void activateEnableButton(int enable);
+	CConfigData * controlsToConfig();
+	void configToControls(CConfigData *configData);
+	void enableControls();
+	void disableControls();
+	CConfigDialog(CMod_sorterApp *app,CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CConfigDialog)
+	enum { IDD = IDD_CONFIG };
+	CListBox	m_Bag[8];
+	CButton		m_BagIn[8];
+	CButton		m_BagOut[8];
+	CComboBox	m_sortItemList;
+	CButton		m_enable;
+	//}}AFX_DATA
+
+	void reloadSortItems();
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CConfigDialog)
+	public:
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CConfigDialog)
+	virtual void OnOK();
+	afx_msg void OnClose();
+	afx_msg void OnEnable();
+	afx_msg void OnTimer(UINT nIDEvent);
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSortEntryAdd1();
+	afx_msg void OnSortEntryRemove1();
+	afx_msg void OnSortEntryAdd2();
+	afx_msg void OnSortEntryRemove2();
+	afx_msg void OnSortEntryAdd3();
+	afx_msg void OnSortEntryRemove3();
+	afx_msg void OnSortEntryAdd4();
+	afx_msg void OnSortEntryRemove4();
+	afx_msg void OnSortEntryAdd5();
+	afx_msg void OnSortEntryRemove5();
+	afx_msg void OnSortEntryAdd6();
+	afx_msg void OnSortEntryRemove6();
+	afx_msg void OnSortEntryAdd7();
+	afx_msg void OnSortEntryRemove7();
+	afx_msg void OnSortEntryAdd8();
+	afx_msg void OnSortEntryRemove8();
+
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CMod_sorterApp * m_app;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_)
