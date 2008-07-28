e4114942a944f13709d19d1232787de54f44646e
Akilez <akilez@example.com>
2008-07-28 09:24:11 +0000

    New module: Auto Banker Current state: Beta

---
+++ mods/mod_banker/ConfigDialog.h
@@ -0,0 +1,75 @@
+#if !defined(AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_)
+#define AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ConfigDialog.h : header file
+//
+
+#include "mod_banker.h"
+#include "ConfigData.h"
+
+struct banker{
+	char name[40];
+	int xPos[10];
+	int yPos[10];
+	int zPos[10];
+};
+
+class CMod_bankerApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CConfigDialog dialog
+
+class CConfigDialog : public CDialog
+{
+// Construction
+public:
+	void activateEnableButton(int enable);
+	CConfigData * controlsToConfig();
+	void configToControls(CConfigData *configData);
+	void enableControls();
+	void disableControls();
+	CConfigDialog(CMod_bankerApp *app,CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CConfigDialog)
+	enum { IDD = IDD_CONFIG };
+	CComboBox m_Banker;
+	CEdit m_MinGold;
+	CButton	m_enable;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CConfigDialog)
+	public:
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CConfigDialog)
+	virtual void OnOK();
+	afx_msg void OnClose();
+	afx_msg void OnEnable();
+	afx_msg void OnTimer(UINT nIDEvent);
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+private:
+	void reloadBankers();
+	CMod_bankerApp * m_app;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_CONFIGDIALOG_H__A38E1144_4FC4_4130_9D8B_A22DE0571AB2__INCLUDED_)
---
+++ mods/mod_banker/exports.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+#include "mod_banker.h"
+
+CMod_bankerApp module;
+#include <IModuleInterface.cpp>
