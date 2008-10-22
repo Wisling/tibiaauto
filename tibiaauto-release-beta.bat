96f2534b91f7e23e783666945f04bf3da311990b
Vanitas <vanitas@example.com>
2008-10-22 18:54:27 +0000

    *** empty log message ***

--- tibiaauto-release-beta.bat
+++ tibiaauto-release-beta.bat
@@ -1,4 +1,14 @@
 e:
+cd \projects\tibiaauto-pub
+cvs update
+cd \projects\tibiaauto-kernel
+cvs update
+d:
+cd d:\Program Files\Microsoft Visual Studio\Common\msdev98\Bin\
+msdev e:\projects\tibiaauto-kernel\tibiaauto.dsw /MAKE "tibiaauto - Win32 Release" /REBUILD
+e:
 cd e:\projects\tibiaauto-pub
+rm -f TibiaAutoSetup_BETA.exe
 "d:\Program Files\NSIS\makensis.exe" tibiaauto-beta.nsi
 "d:\Program Files\WinSCP\WinSCP.com" /script=tibiaauto-release-beta.com
+pause
