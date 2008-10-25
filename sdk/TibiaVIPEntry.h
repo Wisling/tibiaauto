091d77eba7b701e2351a65f3b5e75e79ceffbd32
Akilez <akilez@example.com>
2008-10-25 09:15:23 +0000

    Read Tibia Vip entry method fixed and finished

--- sdk/TibiaVIPEntry.h
+++ sdk/TibiaVIPEntry.h
@@ -2,8 +2,8 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-//#if !defined(AFX_TIBIACONTAINER_H__F994C191_09E2_497B_B20A_FF311B86858B__INCLUDED_)
-//#define AFX_TIBIACONTAINER_H__F994C191_09E2_497B_B20A_FF311B86858B__INCLUDED_
+#if !defined(AFX_TIBIAVIPENTRY_H__F994C191_09E2_497B_B20A_FF311B86858B__INCLUDED_)
+#define AFX_TIBIAVIPENTRY_H__F994C191_09E2_497B_B20A_FF311B86858B__INCLUDED_
 
 #if _MSC_VER > 1000
 #pragma once
@@ -21,4 +21,4 @@ public:
 	int icon;
 };
 
-//#endif
+#endif
