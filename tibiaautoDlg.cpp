f37f5338affe666f476bce6123e05e57c8256f16
Vanitas <vanitas@example.com>
2008-12-01 19:45:20 +0000

    release 1.19.0

--- tibiaautoDlg.cpp
+++ tibiaautoDlg.cpp
@@ -1102,7 +1102,7 @@ void CTibiaautoDlg::OnOptions()
 
 void CTibiaautoDlg::refreshAds()
 {		
-	m_browserAds.Navigate("http://ads.tibiaauto.net/showad.php?version=1.18.0",NULL,NULL,NULL,NULL);
+	m_browserAds.Navigate("http://ads.tibiaauto.net/showad.php?version=1.19.0",NULL,NULL,NULL,NULL);
 }
 
 void CTibiaautoDlg::OnToolLogin() 
@@ -1165,7 +1165,7 @@ void CTibiaautoDlg::reportUsage()
 		int count=CModuleProxy::allModulesCount;
 		int pos;
 		int checksum=tm%177;
-		fprintf(f,"version=1.18.0,tm=%d,",tm);
+		fprintf(f,"version=1.19.0,tm=%d,",tm);
 		for (pos=0;pos<count;pos++)
 		{
 			CModuleProxy *mod=CModuleProxy::allModules[pos];
