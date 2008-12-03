cd258d35be626b9bef5b5acbaa153dd53f6dd299
Vanitas <vanitas@example.com>
2008-12-03 19:39:33 +0000

    1.19.1

--- mods/mod_cavebot/mod_cavebot.cpp
+++ mods/mod_cavebot/mod_cavebot.cpp
@@ -1130,61 +1130,11 @@ DWORD WINAPI toolThreadProc( LPVOID lpParam ) {
 
 
 
-	HHOOK hhookKeyb=NULL;
-	HINSTANCE hinstDLL=NULL;
-	if (config->debug)
-	{
-		hinstDLL = LoadLibrary((LPCTSTR) "tibiaautoinject3.dll");
-		
-		if (hinstDLL)
-		{		
-			typedef LRESULT (CALLBACK *KeyboardProc_fun)(int nCode, WPARAM wParam, LPARAM lParam);
-			static KeyboardProc_fun fun=(KeyboardProc_fun)GetProcAddress(hinstDLL,"KeyboardProc");				
-			if (fun)
-			{									
-				DWORD tibiaProcessId = reader.getProcessId();				
-				HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
-				THREADENTRY32 te32;
-				
-				// Take a snapshot of all running threads
-				hThreadSnap = CreateToolhelp32Snapshot( TH32CS_SNAPTHREAD, 0 );
-				if( hThreadSnap == INVALID_HANDLE_VALUE )
-					return( FALSE );
-				
-				// Fill in the size of the structure before using it.
-				te32.dwSize = sizeof(THREADENTRY32 );
-				
-				// Retrieve information about the first thread,
-				// and exit if unsuccessful
-				if( !Thread32First( hThreadSnap, &te32 ) )
-				{		
-					CloseHandle( hThreadSnap ); // Must clean up the snapshot object!
-					return( FALSE );
-				}
-				
-				// Now walk the thread list of the system,
-				// and display information about each thread
-				// associated with the specified process				
-				do
-				{
-					
-					if( te32.th32OwnerProcessID == tibiaProcessId )
-					{																		
-						hhookKeyb=SetWindowsHookEx(WH_KEYBOARD,fun,hinstDLL,te32.th32ThreadID);
-						break;
-					}
-				} while( Thread32Next(hThreadSnap, &te32 ) );					
-				
-				// Don't forget to clean up the snapshot object.
-				CloseHandle( hThreadSnap );				
-				// initialise hooks								
-			}						
-		}	
-	}
+	
 	
 	while (!toolThreadShouldStop) {
 		Sleep(250);
-		if (reader.getConnectionState()!=8)
+		if (reader.getConnectionState()!=8||!config->pausingEnable)
 		{
 			// flush IPC communication if not logged
 			while (backPipe.readFromPipe(&mess,1009)) {};
@@ -1916,16 +1866,7 @@ DWORD WINAPI toolThreadProc( LPVOID lpParam ) {
 		} // if (no currentlyAttackedCreatureNr)
 
 	}
-
-	if (hhookKeyb)
-	{
-		UnhookWindowsHookEx(hhookKeyb);
-	}	
-
-	if (hinstDLL)
-	{
-		FreeLibrary(hinstDLL);
-	}
+	
 
 	if (shareAlienBackattack) {
 		int zero=0;
