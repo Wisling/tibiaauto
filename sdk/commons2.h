06ab3a6b14582a57f1b3dba4151e09a77a728631
Wisling <wisling@example.com>
2009-07-08 01:01:36 +0000

    Edited itemOnTopIndex to return -1 and made all uses of itemOnTopIndex  handle this

--- sdk/commons2.h
+++ sdk/commons2.h
@@ -8,7 +8,12 @@ int itemOnTopIndex(int x,int y,int z)//Now uses Tibia's own indexing system foun
 	int pos;
 
 	int stackCount=reader.mapGetPointItemsCount(point(x,y,z));
-	if (stackCount==0) AfxMessageBox("Fatal Error: Tile does not exist.");
+	if (stackCount==0){
+		return -1;//return value instead of error(most internal cases handle a return value of -1
+		char buf[111];
+		sprintf (buf,"Fatal Error: Tile does not exist:%d,%d,%d",x,y,z);
+		AfxMessageBox(buf);
+	}
 	for (pos=0;pos<stackCount;pos++)
 	{
 		int stackInd=reader.mapGetPointStackIndex(point(x,y,z),pos);
@@ -29,6 +34,8 @@ int isItemOnTop(int x,int y,int *itemArr,int itemArrSize)
 {
 	CMemReader reader;
 	int topPos=itemOnTopIndex(x,y);
+	if (topPos==-1) return 0;
+
 	int tileId = reader.mapGetPointItemId(point(x,y,0),topPos);
 	for (int i=0;i<itemArrSize;i++)
 	{
@@ -42,6 +49,8 @@ int isItemCovered(int x,int y,int *itemArr,int itemArrSize)
 	CMemReader reader;
 
 	int topPos=itemOnTopIndex(x,y);
+	if (topPos==-1) return 0;
+
 	int stackCount=reader.mapGetPointItemsCount(point(x,y,0));
 	for (int pos=topPos+1;pos!=topPos;pos=(pos+1)%stackCount)
 	{
@@ -107,6 +116,9 @@ int itemSeenOnTopCode(int x,int y)
 int itemOnTopQty(int x,int y)
 {
 	CMemReader reader;
+	int pos=itemOnTopIndex(x,y);
+	if (pos==-1) return 0;
+
 	int qty=reader.mapGetPointItemExtraInfo(point(x,y,0),itemOnTopIndex(x,y),1);
 	return qty?qty:1;
 }
