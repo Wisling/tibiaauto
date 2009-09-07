8ae378cfde3c2c3fed70de8b4ac05ae50002c291
Wisling <wisling@example.com>
2009-09-07 05:31:54 +0000

    disabled map debug for release

--- sdk/ModuleUtil.cpp
+++ sdk/ModuleUtil.cpp
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include "TibiaItemProxy.h"
 #include "PackSenderProxy.h"
 
-#define MAPDEBUG
+//#define MAPDEBUG
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
