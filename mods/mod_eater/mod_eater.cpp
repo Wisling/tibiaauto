1a5c3ec69c047af2161f2a98006009d4da60f385
Akilez <akilez@example.com>
2009-10-11 10:53:36 +0000

    Fixed the randomization attributes of the module. Now it truly eats in acordance with a +/-10% variance of the digestion time.

--- mods/mod_eater/mod_eater.cpp
+++ mods/mod_eater/mod_eater.cpp
@@ -30,6 +30,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include "MemConstData.h"
 #include "TibiaItemProxy.h"
 #include "ModuleUtil.h"
+#include <stdlib.h>  //Includes rand() and srand() functions
+#include <time.h> //Includes timd(int) function for random number seeding
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -78,13 +80,22 @@ END_MESSAGE_MAP()
 // Tool functions
 
 int RandomTimeEaterWait(int digestTime){
-	return CModuleUtil::randomFormula(digestTime,(int)(digestTime*.1),1000000);
+	/***************************
+	rand()							:: Random number between 0 and RAND_MAX (?32767?)
+	digestTime * .2					:: 20% of digestTime
+	rand() % (int)(digestTime * .2)	:: Pseudo-random number between 0 and 20% of digestTime
+	digestTime * .9					:: 90% of digestTime...
+									   When added to a random number from 0 to 20% of digestTime, 
+									   this creates a random number from 90 to 110% (+/- 10%)
+	***************************/
+	return rand() % (int)(digestTime * .2) + (int)(digestTime * .9);
 }
 
 int RandomTimeEaterAmount(){//returns how much food to eat
-	int ans =1;
-	while (rand()%100<60) ans++;
-	return ans;
+	//int ans =1;
+	//while (rand()%100<60) ans++;	//A really round about way of getting a random range of 1 to 60
+	//return ans;
+	return rand() % 5 + 1;			//Do we need to eat more than 5 items at any one time?
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -93,10 +104,9 @@ int RandomTimeEaterAmount(){//returns how much food to eat
 int toolThreadShouldStop=0;
 HANDLE toolThreadHandle;
 
-
-
 DWORD WINAPI toolThreadProc( LPVOID lpParam )
 {			
+	srand(time(NULL));				//Seed the random number generation
 	CMemReaderProxy reader;
 	CPackSenderProxy sender;
 	CMemConstData memConstData = reader.getMemConstData();
@@ -108,55 +118,51 @@ DWORD WINAPI toolThreadProc( LPVOID lpParam )
 	while (!toolThreadShouldStop)
 	{			
 		int pos;
-		if (digestTime==-1) digestTime=0;
-		else {
+		if (digestTime!=-1)		//Simplified the if-statement since digestTime will be zeroed out in a second. Now it is only 0 if we are connected and the module SHOULD run.
     		CModuleUtil::sleepWithStop(RandomTimeEaterWait(digestTime ? digestTime * 1000 : 12000), &toolThreadShouldStop);
-        }
 		if (reader.getConnectionState()!=8) continue; // do not proceed if not connected
-		if (toolThreadShouldStop) continue;
+		//Redundant check on toolThreadShouldStop removed
 
 		digestTime=0;
 		
+		CTibiaItem *foodItem;
+		CTibiaCharacter *self;
 		for (int i=RandomTimeEaterAmount();i>0;i--){
-			CTibiaItem *foodItem;
-			CTibiaCharacter *self = reader.readSelfCharacter();
 			int foodContainer;
 
-			foodItem=NULL;
+			foodItem=NULL;		//foodItem is NULLed during each iteration.
+								//Mo need to delete and re-initialize each time, let's do it at the end
 			foodContainer=-1;
 						
-			
 			CUIntArray *foods=itemProxy.getItemsFoodArray();
 			for (pos=0;pos<memConstData.m_memMaxContainers&&foodItem==NULL;pos++)		
 			{
 				foodItem = CModuleUtil::lookupItem(pos,foods);
 				foodContainer = pos;
 			}
-			//taken care of. delete foods;
-
 			
 			if (foodItem!=NULL)
 			{
+				//self=reader.readSelfCharacter(); //This is the only time the self variable was ever used in the algorithim. self2 was removed.
+				//The previous line was commented out until a fix for waitForCapsChange could be found.
 				sender.useItemInContainer(foodItem->objectId,0x40+foodContainer,foodItem->pos);
-				
-				CTibiaCharacter* self2=reader.readSelfCharacter();
-				if (CModuleUtil::waitForCapsChange(self2->cap))
-					digestTime += itemProxy.getExtraInfo(itemProxy.getIndex(foodItem->objectId, 2), 2);
-				delete self2;
-				char buf[111];
+				//if (CModuleUtil::waitForCapsChange(self->cap)) //TA does not seem to be "catching" the change
+					digestTime += itemProxy.getExtraInfo(itemProxy.getIndex(foodItem->objectId, 2), 2); 
+				//Without the if-statement we are assuming that ALL food we attempt to eat will be eaten
+				//BAD assumption but none other is working ATM!!!
 				if (i!=1)
 					Sleep(CModuleUtil::randomFormula(400,100));
-				delete foodItem;
-				foodItem = NULL;
 			}
-			delete self;
 		}
+		foodItem = NULL;
+		delete foodItem;
+		self = NULL;
+		delete self;
 	}	
 
 	toolThreadShouldStop=0;
 	return 0;
 }
-
 
 /////////////////////////////////////////////////////////////////////////////
 // CMod_eaterApp construction
@@ -174,16 +180,14 @@ CMod_eaterApp::~CMod_eaterApp()
 
 char * CMod_eaterApp::getName()
 {
-	return "Food eater";
+	return "Food Eater";
 }
-
 
 int CMod_eaterApp::isStarted()
 {
 	return m_started;
 }
 
- 
 void CMod_eaterApp::start()
 {	
 	superStart();
@@ -203,10 +207,9 @@ void CMod_eaterApp::stop()
 	m_started=0;		
 } 
 
-
 char *CMod_eaterApp::getVersion()
 {
-	return "1.0";
+	return "2.0";
 }
 
 
