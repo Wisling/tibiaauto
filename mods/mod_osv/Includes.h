3cafc55a213f46d808f9d1688d8e036f6c1c2948
Akilez <akilez@example.com>
2010-02-23 08:10:00 +0000

    OSV import

---
+++ mods/mod_osv/Includes.h
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+
+#include "mod_osv.h"
+
+
+
+#include "ConfigData.h"
+
+#include "TibiaContainer.h"
+
+
+
+#include "MemReaderProxy.h"
+
+#include "PackSenderProxy.h"
+
+#include "ModuleUtil.h"
+
+#include "MemConstData.h"
+
+#include "TibiaItemProxy.h"
+
+#include "ModuleUtil.h"
+
+#include <time.h>
+
+
+
+#include "commons.h"
+
+#ifdef _DEBUG
+
+#define new DEBUG_NEW
+
+#undef THIS_FILE
+
+static char THIS_FILE[] = __FILE__;
+
+#endif
---
+++ mods/mod_osv/exports.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+
+
+#include "mod_osv.h"
+
+
+
+CMod_OSVApp module;
+
+
+
+#include <IModuleInterface.cpp>
