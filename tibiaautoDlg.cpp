ed27fd1efb6233ef613deee37cd97adb5fec439f
Wisling <wisling@example.com>
2010-04-09 04:36:06 +0000

    added string table to tibiaauto

--- tibiaautoDlg.cpp
+++ tibiaautoDlg.cpp
@@ -1396,7 +1396,7 @@ void CTibiaautoDlg::reportUsage()
 		int count=CModuleProxy::allModulesCount;
 		int pos;
 		int checksum=tm%177;
-		fprintf(f,"version=2.2.1,tm=%d,",tm);
+		fprintf(f,"version=2.2.2,tm=%d,",tm);
 		for (pos=0;pos<count;pos++)
 		{
 			CModuleProxy *mod=CModuleProxy::allModules[pos];
