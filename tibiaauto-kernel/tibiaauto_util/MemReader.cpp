Wisling/tibiaauto

commit 15b69b7ffc8285a8db70b8440188395036b8c671

Partially eliminated lots of redundant dynamic memory allocations and deallocations when reading creatures. Still to be done: Optimize out ReadProcessMemory calls. They take ton of CPU time.

tibiaauto-kernel/tibiaauto_util/MemReader.cpp
@@ -217,10 +217,9 @@ CTibiaContainer *CMemReader::readContainer(int containerNr)
 	return container;
 }
 
-CTibiaCharacter *CMemReader::readSelfCharacter()
+void CMemReader::readSelfCharacter(CTibiaCharacter* ch)
 {
-	CTibiaCharacter *ch = new CTibiaCharacter();
-
+	ch->initialized = true;
 	ch->hp      = CMemUtil::GetMemIntValue(m_memAddressHP) ^ CMemUtil::GetMemIntValue(m_memAddressXor);
 	ch->mana    = CMemUtil::GetMemIntValue(m_memAddressMana) ^ CMemUtil::GetMemIntValue(m_memAddressXor);
 	ch->maxHp   = CMemUtil::GetMemIntValue(m_memAddressHPMax) ^ CMemUtil::GetMemIntValue(m_memAddressXor);
@@ -264,8 +263,6 @@ CTibiaCharacter *CMemReader::readSelfCharacter()
 		strncpy(ch->voc, "ed", 3);
 		break;
 	}
-
-
 	ch->skillAxe            = CMemUtil::GetMemIntValue(m_memAddressSkillAxe);
 	ch->skillClub           = CMemUtil::GetMemIntValue(m_memAddressSkillClub);
 	ch->skillSword          = CMemUtil::GetMemIntValue(m_memAddressSkillSword);
@@ -285,35 +282,7 @@ CTibiaCharacter *CMemReader::readSelfCharacter()
 
 	int loggedCharNr = getLoggedCharNr();
 	if (loggedCharNr >= 0)
-	{
-		CTibiaCharacter *monCh = readVisibleCreature(loggedCharNr);
-		ch->tibiaId = monCh->tibiaId;
-		ch->x       = monCh->x;
-		ch->y       = monCh->y;
-		ch->z       = monCh->z;
-		strcpy(ch->name, monCh->name);
-		ch->lookDirection = monCh->lookDirection;
-		ch->colorHead     = monCh->colorHead;
-		ch->colorBody     = monCh->colorBody;
-		ch->colorLegs     = monCh->colorLegs;
-		ch->colorFoot     = monCh->colorFoot;
-		ch->walkSpeed     = monCh->walkSpeed;
-		ch->skulls        = monCh->skulls;
-		ch->shields       = monCh->shields;
-		ch->outfitId      = monCh->outfitId;
-		ch->mountId       = monCh->mountId;
-		ch->monsterType   = monCh->monsterType;
-		ch->hpPercLeft    = monCh->hpPercLeft;
-		ch->lastAttackTm  = monCh->lastAttackTm;
-		ch->nr            = loggedCharNr;
-		ch->warIcon       = monCh->warIcon;
-		ch->blocking      = monCh->blocking;
-		ch->moving        = monCh->moving;
-
-		delete monCh;
-	}
-
-	return ch;
+		readVisibleCreature(ch, loggedCharNr);
 }
 
 CTibiaItem * CMemReader::readItem(int locationAddress)
@@ -332,15 +301,14 @@ CTibiaItem * CMemReader::readItem(int locationAddress)
 	return item;
 }
 
-CTibiaCharacter * CMemReader::readVisibleCreature(int nr)
+void CMemReader::readVisibleCreature(CTibiaCharacter* ch, int nr)
 {
-	CTibiaCharacter *ch = new CTibiaCharacter();
-
 	int offset = m_memAddressFirstCreature + nr * m_memLengthCreature;
 	char memcharinfo[1024];
 	memset(memcharinfo, 0, 1024);
-	CMemUtil::GetMemRange(offset, offset + m_memLengthCreature, memcharinfo, 1);
 
+	CMemUtil::GetMemRange(offset, offset + m_memLengthCreature, memcharinfo, 1);
+	ch->initialized = true;
 	ch->hp       = -1;
 	ch->mana     = -1;
 	ch->tibiaId  = *((int*)memcharinfo);
@@ -404,8 +372,6 @@ CTibiaCharacter * CMemReader::readVisibleCreature(int nr)
 	ch->nr = nr;
 
 	CMemUtil::GetMemRange(offset + 4, offset + 4 + 31, ch->name);
-
-	return ch;
 }
 
 int CMemReader::readBattleListMin()
@@ -471,7 +437,7 @@ int CMemReader::getNextPacketCount()
 	return ret;
 }
 
-CTibiaCharacter *CMemReader::getCharacterByTibiaId(int tibiaId)
+bool CMemReader::getCharacterByTibiaId(CTibiaCharacter * ch, int tibiaId)
 {
 	int i;
 	for (i = 0; i < m_memMaxCreatures; i++)
@@ -479,9 +445,12 @@ CTibiaCharacter *CMemReader::getCharacterByTibiaId(int tibiaId)
 		int offset = m_memAddressFirstCreature + i * m_memLengthCreature;
 
 		if (CMemUtil::GetMemIntValue(offset) == tibiaId)
-			return readVisibleCreature(i);
+		{
+			readVisibleCreature(ch, i);
+			return true;
+		}
 	}
-	return NULL;
+	return false;
 }
 
 int CMemReader::getTradeCountSelf()
@@ -645,11 +614,11 @@ int CMemReader::mapGetSelfCellNr()
 {
 	static int prevSelfTileNr  = 0;
 	static int prevSelfTilePos = 0;
-	int wouldReturn            = 0;
-	CTibiaCharacter *self      = readSelfCharacter();
-	if (self->z < 0 || self->z > 15)
+	int wouldReturn = 0;
+	CTibiaCharacter self;
+	readSelfCharacter(&self);
+	if (self.z < 0 || self.z > 15)
 	{
-		delete self;
 		return 0;
 	}
 
@@ -660,9 +629,8 @@ int CMemReader::mapGetSelfCellNr()
 	if (prevSelfTilePos < c && tileId == 99)
 	{
 		int tileCharId = maptile->items[prevSelfTilePos].quantity;
-		if (tileCharId == self->tibiaId)
+		if (tileCharId == self.tibiaId)
 		{
-			delete self;
 			delete maptile;
 			return prevSelfTileNr;
 		}
@@ -671,7 +639,7 @@ int CMemReader::mapGetSelfCellNr()
 	maptile = NULL;
 
 	int floorSize     = m_memMaxMapTiles / 8;
-	int tileNrLowest  = max(0, min(1768, (self->z <= 7) ? floorSize * (7 - self->z) : (floorSize * 2)));
+	int tileNrLowest  = max(0, min(1768, (self.z <= 7) ? floorSize * (7 - self.z) : (floorSize * 2)));
 	int tileNrHighest = tileNrLowest + floorSize;
 /*
         // check using one of 8 memory locations for current cell(this makes the function 100 times faster)
@@ -688,9 +656,8 @@ int CMemReader::mapGetSelfCellNr()
                         if (tileId==99)
                         {
                                 int tileCharId = CMemUtil::GetMemIntValue(dereference(m_memAddressMapStart)+cell*m_memLengthMapTile+pos*12+4+4);
-                                if (tileCharId==self->tibiaId)
+                                if (tileCharId==self.tibiaId)
                                 {
-                                        delete self;
                                         prevSelfTileNr=cell;
                                         prevSelfTilePos=pos;
                                         return cell;
@@ -713,9 +680,8 @@ int CMemReader::mapGetSelfCellNr()
 			if (tileId == 99)
 			{
 				int tileCharId = maptile->items[pos].quantity;
-				if (tileCharId == self->tibiaId)
+				if (tileCharId == self.tibiaId)
 				{
-					delete self;
 					delete maptile;
 					prevSelfTileNr  = tileNr;
 					prevSelfTilePos = pos;
@@ -739,9 +705,8 @@ int CMemReader::mapGetSelfCellNr()
 			if (tileId == 99)
 			{
 				int tileCharId = maptile->items[pos].quantity;
-				if (tileCharId == self->tibiaId)
+				if (tileCharId == self.tibiaId)
 				{
-					delete self;
 					delete maptile;
 					prevSelfTileNr  = tileNr;
 					prevSelfTilePos = pos;
@@ -752,7 +717,6 @@ int CMemReader::mapGetSelfCellNr()
 		delete maptile;
 		maptile = NULL;
 	}
-	delete self;
 	return 0;
 }
 

tibiaauto-kernel/tibiaauto_util/TibiaCharacter.cpp
@@ -17,6 +17,7 @@ static char THIS_FILE[] = __FILE__;
 
 CTibiaCharacter::CTibiaCharacter()
 {
+	initialized = false;
 	visible = 0;
 	memset(name, 0x00, 32);
 }

tibiaauto-pub/mods/mod_playerinfo/CharInfoDialog.cpp
@@ -108,7 +108,8 @@ END_MESSAGE_MAP()
 void CCharInfoDialog::resetCounters()
 {
 	CMemReader& reader = CMemReader::getMemReader();
-	CTibiaCharacter *ch = reader.readSelfCharacter();
+	CTibiaCharacter ch;
+	 reader.readSelfCharacter(&ch);
 
 	playerInfo.timeStart = time(NULL);
 
@@ -124,31 +125,29 @@ void CCharInfoDialog::resetCounters()
 		expForLvlDiff = ((2 + lvl) * (3 + lvl) / 2 - 4 * (lvl + 1) + 3) * 100;
 		expForLvl    += expForLvlDiff;
 
-		if (expForLvl > ch->exp)
+		if (expForLvl > ch.exp)
 		{
-			expLeft    = expForLvl - ch->exp;
+			expLeft    = expForLvl - ch.exp;
 			percForLvl = MulDiv(expLeft, 100, expForLvlDiff);
 			break;
 		}
 	}
 
-	playerInfo.expStart                      = ch->exp;
-	playerInfo.skill[SKILL_LEVEL].start      = ch->lvl * 100 + (100 - percForLvl);
-	playerInfo.skill[SKILL_MAGICLEVEL].start = ch->mlvl * 100 + (100 - ch->mlvlPercLeft);
-	playerInfo.skill[SKILL_FIST].start       = ch->skillFist * 100 + (100 - ch->skillFistPercLeft);
-	playerInfo.skill[SKILL_CLUB].start       = ch->skillClub * 100 + (100 - ch->skillClubPercLeft);
-	playerInfo.skill[SKILL_SWORD].start      = ch->skillSword * 100 + (100 - ch->skillSwordPercLeft);
-	playerInfo.skill[SKILL_AXE].start        = ch->skillAxe * 100 + (100 - ch->skillAxePercLeft);
-	playerInfo.skill[SKILL_DIST].start       = ch->skillDist * 100 + (100 - ch->skillDistPercLeft);
-	playerInfo.skill[SKILL_SHIELD].start     = ch->skillShield * 100 + (100 - ch->skillShieldPercLeft);
-	playerInfo.skill[SKILL_FISH].start       = ch->skillFish * 100 + (100 - ch->skillFishPercLeft);
+	playerInfo.expStart                      = ch.exp;
+	playerInfo.skill[SKILL_LEVEL].start      = ch.lvl * 100 + (100 - percForLvl);
+	playerInfo.skill[SKILL_MAGICLEVEL].start = ch.mlvl * 100 + (100 - ch.mlvlPercLeft);
+	playerInfo.skill[SKILL_FIST].start       = ch.skillFist * 100 + (100 - ch.skillFistPercLeft);
+	playerInfo.skill[SKILL_CLUB].start       = ch.skillClub * 100 + (100 - ch.skillClubPercLeft);
+	playerInfo.skill[SKILL_SWORD].start      = ch.skillSword * 100 + (100 - ch.skillSwordPercLeft);
+	playerInfo.skill[SKILL_AXE].start        = ch.skillAxe * 100 + (100 - ch.skillAxePercLeft);
+	playerInfo.skill[SKILL_DIST].start       = ch.skillDist * 100 + (100 - ch.skillDistPercLeft);
+	playerInfo.skill[SKILL_SHIELD].start     = ch.skillShield * 100 + (100 - ch.skillShieldPercLeft);
+	playerInfo.skill[SKILL_FISH].start       = ch.skillFish * 100 + (100 - ch.skillFishPercLeft);
 
 	playerInfo.spell[SPELL_INVISIBLE].lasts   = 200;
 	playerInfo.spell[SPELL_HASTE].lasts       = 30;
 	playerInfo.spell[SPELL_STRONGHASTE].lasts = 20;
 	playerInfo.spell[SPELL_SHIELD].lasts      = 200;
-
-	delete ch;
 }
 
 void CCharInfoDialog::DoSetButtonSkin()
@@ -197,7 +196,8 @@ void CCharInfoDialog::OnResetCounters()
 void CCharInfoDialog::dataCalc()
 {
 	CMemReader& reader = CMemReader::getMemReader();
-	CTibiaCharacter *ch = reader.readSelfCharacter();
+	CTibiaCharacter ch;
+	 reader.readSelfCharacter(&ch);
 
 	//T4: Exp calculation
 	long lvl;
@@ -212,9 +212,9 @@ void CCharInfoDialog::dataCalc()
 		expForLvlDiff = ((2 + lvl) * (3 + lvl) / 2 - 4 * (lvl + 1) + 3) * 100;
 		expForLvl    += expForLvlDiff;
 
-		if (expForLvl > ch->exp)
+		if (expForLvl > ch.exp)
 		{
-			expLeft    = expForLvl - ch->exp;
+			expLeft    = expForLvl - ch.exp;
 			percForLvl = MulDiv(expLeft, 100, expForLvlDiff);
 			break;
 		}
@@ -223,32 +223,32 @@ void CCharInfoDialog::dataCalc()
 	playerInfo.timeCurrent = time(NULL);
 	time_t timeDiff = playerInfo.timeCurrent - playerInfo.timeStart;
 
-	playerInfo.hp         = ch->hp;
-	playerInfo.maxHp      = ch->maxHp;
-	playerInfo.mana       = ch->mana;
-	playerInfo.maxMana    = ch->maxMana;
-	playerInfo.soulPoints = ch->soulPoints;
-	playerInfo.stamina    = ch->stamina;
-	playerInfo.capacity   = ch->cap;
-	strncpy(playerInfo.voc, ch->voc, 3);
+	playerInfo.hp         = ch.hp;
+	playerInfo.maxHp      = ch.maxHp;
+	playerInfo.mana       = ch.mana;
+	playerInfo.maxMana    = ch.maxMana;
+	playerInfo.soulPoints = ch.soulPoints;
+	playerInfo.stamina    = ch.stamina;
+	playerInfo.capacity   = ch.cap;
+	strncpy(playerInfo.voc, ch.voc, 3);
 
-	if (!strcmp(ch->voc, "k") || !strcmp(ch->voc, "ek")) //Knight
-		playerInfo.maxCapacity = 270 + ch->lvl * 25;
-	else if (!strcmp(ch->voc, "p") || !strcmp(ch->voc, "rp")) //Pall
-		playerInfo.maxCapacity = 310 + ch->lvl * 20;
+	if (!strcmp(ch.voc, "k") || !strcmp(ch.voc, "ek")) //Knight
+		playerInfo.maxCapacity = 270 + ch.lvl * 25;
+	else if (!strcmp(ch.voc, "p") || !strcmp(ch.voc, "rp")) //Pall
+		playerInfo.maxCapacity = 310 + ch.lvl * 20;
 	else   //Rest
-		playerInfo.maxCapacity = 390 + ch->lvl * 10;
+		playerInfo.maxCapacity = 390 + ch.lvl * 10;
 
 	//Exp stats
-	playerInfo.expCurrent = ch->exp;
+	playerInfo.expCurrent = ch.exp;
 	playerInfo.expLvl     = expForLvlDiff;
 	playerInfo.expLeft    = expLeft;
 	if (timeDiff)
 		playerInfo.expPerHour = MulDiv(playerInfo.expCurrent - playerInfo.expStart, 3600, (int)timeDiff);
 
 	//Skills
-	unsigned long skillLvl[9]      = {ch->lvl, ch->mlvl, ch->skillFist, ch->skillClub, ch->skillSword, ch->skillAxe, ch->skillDist, ch->skillShield, ch->skillFish};
-	unsigned long skillPercLeft[9] = {percForLvl, ch->mlvlPercLeft, ch->skillFistPercLeft, ch->skillClubPercLeft, ch->skillSwordPercLeft, ch->skillAxePercLeft, ch->skillDistPercLeft, ch->skillShieldPercLeft, ch->skillFishPercLeft};
+	unsigned long skillLvl[9]      = {ch.lvl, ch.mlvl, ch.skillFist, ch.skillClub, ch.skillSword, ch.skillAxe, ch.skillDist, ch.skillShield, ch.skillFish};
+	unsigned long skillPercLeft[9] = {percForLvl, ch.mlvlPercLeft, ch.skillFistPercLeft, ch.skillClubPercLeft, ch.skillSwordPercLeft, ch.skillAxePercLeft, ch.skillDistPercLeft, ch.skillShieldPercLeft, ch.skillFishPercLeft};
 
 	//Lvl
 	playerInfo.skill[SKILL_LEVEL].lvl      = skillLvl[SKILL_LEVEL];
@@ -297,9 +297,9 @@ void CCharInfoDialog::dataCalc()
 		memcpy(msgBuf, mess.payload + 16 + nickLen, msgLen);
 
 
-		if ((infoType == 1) && (strcmp(nickBuf, ch->name) == 0) && (strcmp(nickBuf, "Tibia Auto") != 0))
+		if ((infoType == 1) && (strcmp(nickBuf, ch.name) == 0) && (strcmp(nickBuf, "Tibia Auto") != 0))
 		{
-//			sprintf(buf,"nick: '%s', msg: '%s', name: '%s', type: '%d'", nickBuf, msgBuf, ch->name, infoType);
+//			sprintf(buf,"nick: '%s', msg: '%s', name: '%s', type: '%d'", nickBuf, msgBuf, ch.name, infoType);
 //			CPackSender::sendTAMessage(buf);
 
 			if ((_strcmpi(msgBuf, "utana vid ") == 0) || (strcmp(msgBuf, "test invis") == 0))  //invisible
@@ -343,7 +343,6 @@ void CCharInfoDialog::dataCalc()
 				playerInfo.spell[i].warning |= 0x02;
 		}
 	}
-	delete ch;
 }
 
 void CCharInfoDialog::dataShow()

tibiaauto-kernel/PythonEngine.cpp
@@ -181,7 +181,8 @@ static PyObject *tibiaauto_tibiaauto_registerPlugin(PyObject *self, PyObject *ar
    {
         CMemReader& reader = CMemReader::getMemReader();
 
-        CTibiaCharacter *selfCh = reader.readSelfCharacter();
+        CTibiaCharacter selfCh;
+         reader.readSelfCharacter(&selfCh);
         PyObject *ret =
                 Py_BuildValue("{s:i,s:i,s:i,s:i,s:i}",
                 "hp",selfCh->hp,

commit 7897fe8e9333cf57890bfa41de7bf0e8873de687

Fixed self character hp & mana reading broken in 15b69b7ffc8285a8db70b8440188395036b8c671.

tibiaauto-kernel/tibiaauto_util/MemReader.cpp
@@ -309,8 +309,6 @@ void CMemReader::readVisibleCreature(CTibiaCharacter* ch, int nr)
 
 	CMemUtil::getMemUtil().GetMemRange(offset, offset + m_memLengthCreature, memcharinfo, 1);
 	ch->initialized = true;
-	ch->hp       = -1;
-	ch->mana     = -1;
 	ch->tibiaId  = *((int*)memcharinfo);
 	ch->z        = *((int*)(memcharinfo + 36));
 	ch->y        = *((int*)(memcharinfo + 40));
@@ -367,10 +365,7 @@ void CMemReader::readVisibleCreature(CTibiaCharacter* ch, int nr)
 	//ch->alwayszero=*((int*)(memcharinfo+208));
 	//ch->treepointer2=*((int*)(memcharinfo+212));
 	//ch->alwayszero=*((int*)(memcharinfo+216));
-
-
 	ch->nr = nr;
-
 	CMemUtil::getMemUtil().GetMemRange(offset + 4, offset + 4 + 31, ch->name);
 }
 

tibiaauto-kernel/tibiaauto_util/TibiaCharacter.cpp
@@ -19,7 +19,9 @@ CTibiaCharacter::CTibiaCharacter()
 {
 	initialized = false;
 	visible = 0;
-	memset(name, 0x00, 32);
+	name[0] = 0x00;
+	hp = -1;
+	mana = -1;
 }
 
 CTibiaCharacter::~CTibiaCharacter()

tibiaauto-pub/mods/mod_playerinfo/CharInfoDialog.cpp
@@ -197,7 +197,7 @@ void CCharInfoDialog::dataCalc()
 {
 	CMemReader& reader = CMemReader::getMemReader();
 	CTibiaCharacter ch;
-	 reader.readSelfCharacter(&ch);
+	reader.readSelfCharacter(&ch);
 
 	//T4: Exp calculation
 	long lvl;